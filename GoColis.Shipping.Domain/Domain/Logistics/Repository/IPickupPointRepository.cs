0b851f00598228f3744e59e23617038b0f7bd875
ayoub <ayoubzoghlami517@gmail.com>
2023-02-24 17:38:09 +0100

    new

--- GoColis.Shipping.Domain/Domain/Logistics/Repository/IPickupPointRepository.cs
+++ GoColis.Shipping.Domain/Domain/Logistics/Repository/IPickupPointRepository.cs
@@ -1,8 +1,9 @@
-﻿using GoColis.Shipping.Domain.Domain.Logistics.Agregat;
+﻿using GoColis.Shipping.Domain.Common;
+using GoColis.Shipping.Domain.Domain.Logistics.Agregat;
 
 namespace GoColis.Shipping.Domain.Domain.Logistics.Repository;
 
-public interface IPickupPointRepository : IRepository<Entity>
+public interface IPickupPointRepository<class> : IRepository 
 {
     
 }
