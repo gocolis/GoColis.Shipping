54872ed7fa206680a1d37962343ed15ce922cd77
mansourplus <benmedmansour@gmail.com>
2023-02-27 20:38:22 +0100

    Add migrations

---
+++ GoColis.Shpping.Infrastructure/Logistics/Migrations/Contact_20230227191000.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+
+namespace GoColis.Shipping.Infrastructure.Logistics.Migrations;
+
+[Migration(20230227191000)]
+public class Contact_20230227191000 : FluentMigrator.Migration
+{
+    const string tableName = "Contacts";
+    const string index = "IX_CONTACT_PICKUPPOINT";
+    public override void Down()
+    {
+        Delete.Index(index);
+        Delete.Table(tableName);
+    }
+
+    public override void Up()
+    {
+        Create.Table(tableName)
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey()
+            .WithColumn("firstName").AsString().NotNullable()
+            .WithColumn("lastName").AsString().NotNullable()
+            .WithColumn("email").AsString().NotNullable()
+            .WithColumn("phone").AsString().NotNullable()
+            .WithColumn("role").AsDecimal().Nullable()
+            .WithColumn("PickupPointID").AsGuid().NotNullable().ForeignKey("PickupPoint_VS_Contact", "PickupPoints","id")
+            .WithColumn("CreatedAt").AsDateTime().Nullable()
+            .WithColumn("UpdatedAt").AsDateTime().Nullable()
+            .WithColumn("CreatedBy").AsString().Nullable()
+            .WithColumn("UpdatedBy").AsString().Nullable()
+            ;
+
+        Create.Index(index).OnTable(tableName).OnColumn("PickupPointID");
+    }
+}
---
+++ GoColis.Shpping.Infrastructure/Logistics/Migrations/PickupPoint_20230227190000.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace GoColis.Shipping.Infrastructure.Logistics.Migrations;
+
+[Migration(20230227190000)]
+public class PickupPoint_20230227190000 : FluentMigrator.Migration
+{
+    const string tableName = "PickupPoints";
+    public override void Down()
+    {
+        Delete.Table(tableName);
+    }
+
+    public override void Up()
+    {
+        Create.Table(tableName)
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey()
+            .WithColumn("IdSte").AsGuid().NotNullable()
+            .WithColumn("Adresse").AsString().NotNullable()
+            .WithColumn("Latitude").AsDecimal().Nullable()
+            .WithColumn("Longitude").AsDecimal().Nullable()
+            .WithColumn("CreatedAt").AsDateTime().Nullable()
+            .WithColumn("UpdatedAt").AsDateTime().Nullable()
+            .WithColumn("CreatedBy").AsString().Nullable()
+            .WithColumn("UpdatedBy").AsString().Nullable()
+            ;
+    }
+}
