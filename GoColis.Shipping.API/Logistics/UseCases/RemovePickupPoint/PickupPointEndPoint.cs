32618f446877dd1bb27f0f2985c989263c59598c
OussemaTouaibi <oussema.touaibi@esprit.tn>
2023-02-27 13:28:58 -1200

    Updating

--- GoColis.Shipping.API/Logistics/UseCases/RemovePickupPoint/PickupPointEndPoint.cs
+++ GoColis.Shipping.API/Logistics/UseCases/RemovePickupPoint/PickupPointEndPoint.cs
@@ -6,9 +6,8 @@ public static class PickupPointEndPoint
     public static void MapRemovePickupPointEndpoints(this WebApplication app)
     {
         app.MapDelete("/api/pickuppoint/{pickuppointId}",
-              async (HttpRequest req, IMediator mediatr) =>
+              async (string pickuppointId, HttpRequest req, IMediator mediatr) =>
               {
-                  var pickuppointId = req.Query["pickuppointId"];
                   var request = new RemovePickupPointRequestViewModel(pickuppointId);
                   var command = request.ToDomain();
                   var Result = await mediatr.Send(command);
